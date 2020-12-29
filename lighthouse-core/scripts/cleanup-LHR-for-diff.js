55f8fff01953727b03a368542d0e2bf8ae92e1d4
Connor Clark <cjamcl@google.com>
2020-12-29 17:51:24 -0600

    k whatev

--- lighthouse-core/scripts/cleanup-LHR-for-diff.js
+++ lighthouse-core/scripts/cleanup-LHR-for-diff.js
@@ -28,8 +28,8 @@ function cleanAndFormatLHR(lhrString) {
 
   // TODO: Resolve the below so we don't need to force it to a boolean value:
   // 1) The string|boolean story for proto
-  // 2) Travis gets a absolute path during yarn diff:sample-json
-  // lhr.configSettings.auditMode = true; // testing...
+  // 2) CI gets a absolute path during yarn diff:sample-json
+  lhr.configSettings.auditMode = true;
 
   // Set timing values, which change from run to run, to predictable values
   lhr.timing.total = 12345.6789;
